1e3b9dabfe8913b66bc72d7abac75a3dac84f995
Manoj Kumar <manoj@cogoport.com>
2020-02-16 00:54:02 +0530

    new look

---
+++ work.js
@@ -0,0 +1,18 @@
+const work = [
+    {
+        title: "🎓 Cogoport Internship",
+        link: "https://kumarmj.github.io/"
+    },
+    {
+        title: "🎓 Winuall Internship",
+        link: "https://kumarmj.github.io/"
+    },
+    {
+        title: "🚀 Udemy Certificate",
+        link: "https://kumarmj.github.io/"
+    }
+]
+
+const wrkList = document.getElementById("work_list");
+let workItems = [];
+wrkList.innerHTML = work.map(({ title, link }) => `<a class='work_item' href=${link} target="_blank">${title}</div>`);
